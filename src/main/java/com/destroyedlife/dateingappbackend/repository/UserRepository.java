fc7c58054e666de62d34f669527ccf7680c956b6
jaesu <jaesun8674@gmail.com>
2022-04-18 21:36:58 +0900

    UserRepositoryTest 삭제 및 User 도메인 빌더 테스트

--- src/main/java/com/destroyedlife/dateingappbackend/repository/UserRepository.java
+++ src/main/java/com/destroyedlife/dateingappbackend/repository/UserRepository.java
@@ -2,12 +2,9 @@ package com.destroyedlife.dateingappbackend.repository;
 
 import com.destroyedlife.dateingappbackend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.math.BigInteger;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findOneByName(@Param("name") String name);
+
 }
